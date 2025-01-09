a84e47c6abb40f9e7ca9f539d4c9629ce27abb71
Seila-Dev <erickrodrigues3975@gmail.com>
2025-01-08 22:52:46 -0300

    adicionada mensagem de erro na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,9 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.get("/movies", async (req, res) => {
+app.use(express.json());
+
+app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
             title: "asc",
@@ -16,6 +18,26 @@ app.get("/movies", async (req, res) => {
         },
     });
     res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+    try {
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date)
+            }
+        });
+    } catch (error) {
+        return res.status(500).send({message: "Falha no servidor"});
+    }
+
+    res.status(201).send();
 });
 
 app.listen(port, () => {
